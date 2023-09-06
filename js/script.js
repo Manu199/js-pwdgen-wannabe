0282a4db8fb1fa24317af891be03c608a8eb6fdc
Manu199 <manu199gh@gmail.com>
2023-09-06 15:38:27 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/*
+
+1. Chiedere all'utente il suo nome.
+
+2. Chiedere all'utente il suo cognome.
+
+3. Chiedere all'utente il suo colore preferito.
+
+4. Effettuare l'innerHTML dell'output nomecognomecolorepreferito23
+
+*/ 
