10f3208a808249c23618598e540352c14881a8ac
Manu199 <manu199gh@gmail.com>
2023-09-06 17:50:50 +0200

    [add] const (name, lastname and color)

---
+++ README.md
@@ -0,0 +1,9 @@
+Password Generator
+===
+Abstract:xxx
+## Title: Password generator
+- Chiedere all'utente il suo nome.
+- Chiedere all'utente il suo cognome.
+
+- Chiedere all'utente il suo colore preferito.
+- Effettuare l'innerHTML dell'output nomecognomecolorepreferito23
--- js/script.js
+++ js/script.js
@@ -9,3 +9,17 @@
 4. Effettuare l'innerHTML dell'output nomecognomecolorepreferito23
 
 */ 
+
+const outputTag = document.getElementById('output');
+
+
+const name = prompt('Inserire il nome');
+
+const lastname = prompt('Inserire il cognome');
+
+const color = prompt('Inserire il colore preferito');
+
+const message = 'Your' + ' ' + 'Password' + ' ' + '=' + ' ' + name + lastname + color + 23;
+
+
+outputTag.innerHTML = message;  
